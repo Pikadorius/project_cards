b6a9d3dfb2055ceb187a96a4948cd7f94d6c2f5a
Pikadorius <karpukea@yandex.by>
2023-01-24 16:59:13 +0300

    added possibility to return from error page

--- src/components/common/Errors/Error404/Error404.tsx
+++ src/components/common/Errors/Error404/Error404.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import s from './Error404.module.css'
 import error404 from './404.svg'
+import {NavLink} from 'react-router-dom';
 
 const Error404 = () => {
     return (
         <div id={'hw5-page-404'}>
+
             <div className={s.wrapper}>
+                <NavLink to={'/'}>Go to main</NavLink>
                 <img src={error404} alt={'404'} className={s.error404} />
             </div>
         </div>
