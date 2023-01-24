08966ab041d5563d36be803c711e22dddee576bf
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-01-24 19:57:39 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     const profile = useAppSelector(state => state.profilePage)
     const dispatch = useAppDispatch()
 
-// раз раз
+// раз
     return (
         <div className="App">
             <Header/>
