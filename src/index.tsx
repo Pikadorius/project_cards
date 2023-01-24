5bc4b4c37446619e50dd72292dda1bbfead9256d
Pikadorius <karpukea@yandex.by>
2023-01-24 14:50:37 +0300

    changed Hash to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import store from './store/store';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 );
 
