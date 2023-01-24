142e42ae640b36977f2484841ae9ffc7e48615e1
Pikadorius <karpukea@yandex.by>
2023-01-24 14:04:03 +0300

    created header with navigation

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import s from './Header.module.css'
+
+
+export const PATH = {
+    REGISTRATION: `/registration`,
+    LOGIN: `/login`,
+    PROFILE: `/profile`,
+    UNIVERSAL_COMPONENTS: `/universal-components`
+}
+
+const Header = () => {
+    return (
+        <header>
+            <nav className={s.navigation}>
+                <NavLink
+                    to={PATH.PROFILE}
+                    className={({isActive}) => isActive ? s.active : ''}>
+                    Profile
+                </NavLink>
+
+                <NavLink
+                    to={PATH.REGISTRATION}
+                    className={({isActive}) => isActive ? s.active : ''}>
+                    Registration
+                </NavLink>
+
+                <NavLink
+                    to={PATH.LOGIN}
+                    className={({isActive}) => isActive ? s.active : ''}>
+                    Login
+                </NavLink>
+
+                <NavLink
+                    to={PATH.UNIVERSAL_COMPONENTS}
+                    className={({isActive}) => isActive ? s.active : ''}>
+                    Components
+                </NavLink>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
