4b32b749afe3a255242b9dc2d962cb2f17a738fa
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-01-24 20:37:23 +0300

    new comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from './store/store';
-import Header from './components/Header/Header';
-import {Outlet} from 'react-router-dom';
+import Header, {PATH} from './components/Header/Header';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import CommonComponents from './components/common/CommonComponents';
+import Error404 from './components/common/Errors/Error404/Error404';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegistrationPage from './components/RegistrationPage/RegistrationPage';
+import ProfilePage from './components/ProfilePage/ProfilePage';
 
 function App() {
     const reg = useAppSelector(state => state.registrationPage)
@@ -10,21 +15,18 @@ function App() {
     const profile = useAppSelector(state => state.profilePage)
     const dispatch = useAppDispatch()
 
-// туц туц ===============================
-    // test 2
-
+// раз размммммммммммммммммм тарарарар
     return (
         <div className="App">
             <Header/>
-            <Outlet/>
-            {/*<Routes>
+            <Routes>
                 <Route path={'/'} element={<Navigate to={PATH.PROFILE}/>}/>
                 <Route path={PATH.LOGIN} element={<LoginPage/>}/>
                 <Route path={PATH.REGISTRATION} element={<RegistrationPage/>}/>
                 <Route path={PATH.PROFILE} element={<ProfilePage/>}/>
                 <Route path={PATH.UNIVERSAL_COMPONENTS} element={<CommonComponents/>}/>
                 <Route path={"*"} element={<Error404/>}/>
-            </Routes>*/}
+            </Routes>
         </div>
     );
 }
