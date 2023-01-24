a5426b0d93a9acba6eeca8a11fda02d0d845d057
Pikadorius <karpukea@yandex.by>
2023-01-24 12:45:37 +0300

    deleted default code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useAppDispatch, useAppSelector} from './store/store';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const reg = useAppSelector(state => state.registrationPage)
+    const log = useAppSelector(state => state.loginPage)
+    const profile = useAppSelector(state => state.profilePage)
+    const dispatch = useAppDispatch()
+
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
