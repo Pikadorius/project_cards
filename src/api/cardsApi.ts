c52ec9a505290f1b5c7980854bf9d41634fbb69e
Pikadorius <karpukea@yandex.by>
2023-02-03 20:03:01 +0300

    KEA - login button

--- src/api/cardsApi.ts
+++ src/api/cardsApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     headers: {},
     withCredentials: true,
-    baseURL: 'https://'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/'
 })
 
 export const cardsAPI = {
