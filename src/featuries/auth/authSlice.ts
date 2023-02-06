2f1db4148bf8106953138f14bbdeb27ad23c49fd
Pikadorius <karpukea@yandex.by>
2023-02-06 15:41:24 +0300

    added register TC

--- src/featuries/auth/authSlice.ts
+++ src/featuries/auth/authSlice.ts
@@ -1,31 +1,45 @@
-import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
-import { authApi } from "./authApi";
+import {createSlice, PayloadAction, createAsyncThunk} from "@reduxjs/toolkit";
+import {authApi, RegistrationRequestType} from "./authApi";
 
 type InitialStateType = {
-  isLoggedIn: boolean;
+    isLoggedIn: boolean;
 };
 
 export const authMeTC = createAsyncThunk(
-  "isLoggedIn",
-  async (_, { dispatch }) => {
-    const res = await authApi.authMe();
-    try {
-      dispatch(isLoggedIn(true));
-    } catch (e: any) {}
-  }
+    "isLoggedIn",
+    async (_, {dispatch}) => {
+        const res = await authApi.authMe();
+        try {
+            dispatch(isLoggedIn(true));
+        } catch (e: any) {
+        }
+    }
 );
 
+export const registerTC = createAsyncThunk(
+    "isRegistred",
+    async (data: RegistrationRequestType, {dispatch}) => {
+        try {
+            const res = await authApi.register(data)
+            console.log(res)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+)
+
+
 const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    isLoggedIn: false,
-  } as InitialStateType,
-  reducers: {
-    isLoggedIn: (state, action: PayloadAction<boolean>) => {
-      state.isLoggedIn = action.payload;
+    name: "auth",
+    initialState: {
+        isLoggedIn: false,
+    } as InitialStateType,
+    reducers: {
+        isLoggedIn: (state, action: PayloadAction<boolean>) => {
+            state.isLoggedIn = action.payload;
+        },
     },
-  },
 });
 
-const { isLoggedIn } = authSlice.actions;
+const {isLoggedIn} = authSlice.actions;
 export const authReducer = authSlice.reducer;
