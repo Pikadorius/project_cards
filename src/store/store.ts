4de20612720a0c1be5cffe103f123b799d7cb0fb
Pikadorius <karpukea@yandex.by>
2023-02-06 16:29:44 +0300

    added app to state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../featuries/auth/authSlice";
+import {appReducer} from '../app/appSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    app: appReducer
   },
 });
 
