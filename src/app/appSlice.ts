2aef903d94a4092f74bd0756a97ce0c5c358af27
Pikadorius <karpukea@yandex.by>
2023-02-06 15:39:37 +0300

    added appError

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,7 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
+
+
+type InitialStateType = {
+    appError: string | null
+}
+
+const initialState: InitialStateType = {
+    appError: null
+}
 
 export const appSlice = createSlice({
-  name: "app",
-  initialState: {},
-  reducers: {},
+    name: "app",
+    initialState: {},
+    reducers: {},
 });
