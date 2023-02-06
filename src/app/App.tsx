4c2fe97184c2fb8e59b7b0aba6d822c90d9a8d8b
Pikadorius <karpukea@yandex.by>
2023-02-06 16:30:46 +0300

    added error snackbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./App.css";
-import { Header } from "./Header/Header";
+import {Header} from "./Header/Header";
 import Pages from "./Pages/Pages";
-import { authMeTC } from "../featuries/auth/authSlice";
-import { useAppDispatch } from "../common/hooks/AppDispatch";
-import { useAppSelector } from "../common/hooks/AppSelector";
+import {authMeTC} from "../featuries/auth/authSlice";
+import {useAppDispatch} from "../common/hooks/AppDispatch";
+import {useAppSelector} from "../common/hooks/AppSelector";
+import SimpleSnackbar from '../common/components/SnackBar/Snackbar';
 
 function App() {
     // EGOR serg
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(authMeTC());
-  }, []);
-  return (
-    <div className="app">
-      <Header />
-      <Pages />
-    </div>
-  );
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(authMeTC());
+    }, []);
+    return (
+        <div className="app">
+            <Header/>
+            <Pages/>
+            <SimpleSnackbar/>
+        </div>
+    );
 }
 
 export default App;
