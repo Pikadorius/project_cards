2e27bfc12e05dae9925238965222a5b3759f9d6e
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-02-07 19:23:02 +0300

    added new style in EditableSpan and new icon

--- src/app/Header/HeaderProfile/HeaderProfile.tsx
+++ src/app/Header/HeaderProfile/HeaderProfile.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import s from "./HeaderProfile.module.scss";
 import { Link } from "react-router-dom";
 import ava from "../../../assets/ava.png";
-import { useAppDispatch } from "../../../common/hooks/AppDispatch";
+
 import { useAppSelector } from "../../../common/hooks/AppSelector";
 import { PATH } from "../../../common/constans/path";
+import { UserNameHandler } from "../../../common/utils/userNameHandler";
 
 const HeaderProfile = () => {
   const user = useAppSelector((state) => state.auth.user);
+  const userName = UserNameHandler(user.name);
   return (
     <div className={s.profileContainer}>
-      <div className={s.userName}>{user.name}</div>
+      <div className={s.userName}>{userName}</div>
       <Link to={PATH.ACCOUNT} className={s.avatarContainer}>
         <img className={s.userAvatar} src={ava} alt="user avatar" />
       </Link>
