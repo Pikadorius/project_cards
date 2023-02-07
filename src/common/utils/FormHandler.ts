0b08d9ccd7c5245a926e8c9eedf00155fca9f7ac
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-02-07 11:48:58 +0300

    added universal function formHandler

--- src/common/utils/FormHandler.ts
+++ src/common/utils/FormHandler.ts
@@ -2,24 +2,30 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 
-export const formHandler = () => {
-  const formSchema = Yup.object().shape({
-    email: Yup.string()
-      .required("No email provided")
-      .email("Incorrect email")
-      .matches(
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Incorrect email"
-      ),
-    password: Yup.string()
-      .required("No password provided")
-      .matches(/(?=.*\d)(?=.*[a-z]).{8,}/, "Incorrect password"),
-    confirmPwd: Yup.string().oneOf(
-      [Yup.ref("password")],
-      "Passwords does not match"
+const schemaParam = {
+  email: Yup.string()
+    .required("No email provided")
+    .email("Incorrect email")
+    .matches(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Incorrect email"
     ),
-  });
+  password: Yup.string()
+    .required("No password provided")
+    .matches(/(?=.*\d)(?=.*[a-z]).{8,}/, "Incorrect password"),
+  confirmPwd: Yup.string().oneOf(
+    [Yup.ref("password")],
+    "Passwords does not match"
+  ),
+};
 
+export const formHandler = (...keys: string[]) => {
+  const param: any = {};
+  Object.entries(schemaParam).forEach((el, i) =>
+    el.includes(keys[i]) ? (param[el[0]] = el[1]) : null
+  );
+
+  const formSchema = Yup.object().shape(param);
   const {
     register,
     formState: { errors, isValid },
@@ -43,58 +49,59 @@ export const formHandler = () => {
     errorEmail,
     errorPassword,
     errorConfirmPwd,
+    keys,
   };
 };
 
-export const formHandlerRec = () => {
-  const formSchema = Yup.object().shape({
-    email: Yup.string()
-      .required("No email provided")
-      .email("Incorrect email")
-      .min(6, "Incorrect email"),
-  });
-
-  const {
-    register,
-    formState: { errors, isValid },
-    handleSubmit,
-    reset,
-  } = useForm({ resolver: yupResolver(formSchema), mode: "onTouched" });
-
-  const errorEmail = errors.email ? String(errors.email.message) : undefined;
-
-  return {
-    register,
-    handleSubmit,
-    reset,
-    isValid,
-    errorEmail,
-  };
-};
-
-export const formHandlerPass = () => {
-  const formSchema = Yup.object().shape({
-    password: Yup.string()
-      .required("No password provided")
-      .matches(/(?=.*\d)(?=.*[a-z]).{8,}/, "Incorrect password"),
-  });
-
-  const {
-    register,
-    formState: { errors, isValid },
-    handleSubmit,
-    reset,
-  } = useForm({ resolver: yupResolver(formSchema), mode: "onTouched" });
-
-  const errorPassword = errors.password
-    ? String(errors.password.message)
-    : undefined;
-
-  return {
-    register,
-    handleSubmit,
-    reset,
-    isValid,
-    errorPassword,
-  };
-};
+// export const formHandlerRec = () => {
+//   const formSchema = Yup.object().shape({
+//     email: Yup.string()
+//       .required("No email provided")
+//       .email("Incorrect email")
+//       .min(6, "Incorrect email"),
+//   });
+//
+//   const {
+//     register,
+//     formState: { errors, isValid },
+//     handleSubmit,
+//     reset,
+//   } = useForm({ resolver: yupResolver(formSchema), mode: "onTouched" });
+//
+//   const errorEmail = errors.email ? String(errors.email.message) : undefined;
+//
+//   return {
+//     register,
+//     handleSubmit,
+//     reset,
+//     isValid,
+//     errorEmail,
+//   };
+// };
+//
+// export const formHandlerPass = () => {
+//   const formSchema = Yup.object().shape({
+//     password: Yup.string()
+//       .required("No password provided")
+//       .matches(/(?=.*\d)(?=.*[a-z]).{8,}/, "Incorrect password"),
+//   });
+//
+//   const {
+//     register,
+//     formState: { errors, isValid },
+//     handleSubmit,
+//     reset,
+//   } = useForm({ resolver: yupResolver(formSchema), mode: "onTouched" });
+//
+//   const errorPassword = errors.password
+//     ? String(errors.password.message)
+//     : undefined;
+//
+//   return {
+//     register,
+//     handleSubmit,
+//     reset,
+//     isValid,
+//     errorPassword,
+//   };
+// };
