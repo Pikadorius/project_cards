06e1d82ce1b64d5f1f398c1f3a01680c2e8fd95d
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-02-08 18:52:28 +0300

    fix path

--- src/common/constans/path.ts
+++ src/common/constans/path.ts
@@ -2,8 +2,8 @@ export const PATH = {
   LOGIN: '/login',
   REGISTER: '/register',
   RECOVERY: '/recovery',
-  PROFILE: '/profile',
   ACCOUNT: '/account',
   NEW_PASSWORD: '/set-new-password',
   RECOVERY_INFO: '/recovery-info',
+  PACK_LIST: '/pack-list',
 }
