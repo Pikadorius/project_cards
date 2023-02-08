5784639e3e6c22e609b90de795c9bb51b41f6515
Pikadorius <karpukea@yandex.by>
2023-02-08 11:10:21 +0300

    added change appStatus to failed

--- src/common/utils/errorHandler.ts
+++ src/common/utils/errorHandler.ts
@@ -1,17 +1,21 @@
-import { Dispatch } from 'redux';
-import axios, { AxiosError } from 'axios';
-import {setAppError} from '../../app/appSlice';
+import { Dispatch } from "redux";
+import axios, { AxiosError } from "axios";
+import { setAppError, setAppStatus } from "../../app/appSlice";
 // import { AppThunk } from '../../app/store';
 // import { packsAPI } from '../../api/packs-api';
 
-export const errorUtils = (e: Error | AxiosError<{error: string}>, dispatch: Dispatch) => {
-    const err = e as Error | AxiosError<{ error: string }>
-    if (axios.isAxiosError(err)) {
-        const error = err.response?.data ? err.response.data.error : err.message
-        dispatch(setAppError(error))
-        console.log(error)
-    } else {
-        dispatch(setAppError(`Native error ${err.message}`))
-        console.log(err.message)
-    }
-}
+export const errorUtils = (
+  e: Error | AxiosError<{ error: string }>,
+  dispatch: Dispatch
+) => {
+  const err = e as Error | AxiosError<{ error: string }>;
+  if (axios.isAxiosError(err)) {
+    const error = err.response?.data ? err.response.data.error : err.message;
+    dispatch(setAppError(error));
+    console.log(error);
+  } else {
+    dispatch(setAppError(`Native error ${err.message}`));
+    console.log(err.message);
+  }
+  dispatch(setAppStatus("failed"));
+};
