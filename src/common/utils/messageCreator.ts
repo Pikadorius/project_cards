b690b37b584b5b75ac313d171edd0fe406fdf59f
Pikadorius <karpukea@yandex.by>
2023-02-09 09:48:46 +0300

    changed baseURL

--- src/common/utils/messageCreator.ts
+++ src/common/utils/messageCreator.ts
@@ -1,4 +1,5 @@
 import { PATH } from '../constans/path'
+import { recoveryURL } from '../constans/url'
 
 export const createMessage = (email: string) => {
   return `<div style=padding: "15px">
@@ -6,6 +7,6 @@ export const createMessage = (email: string) => {
             <div>There was a request to change your password!</div>
             <div>If you did not make this request then please ignore this email.</div>
             <div>Otherwise, please click this link to change your password: 
-            <a href='http://localhost:3000/#${PATH.NEW_PASSWORD}/$token$'>[link] </a></div>
+            <a href='${recoveryURL}#${PATH.NEW_PASSWORD}/$token$'>[link] </a></div>
             </div>`
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {HashRouter} from 'react-router-dom'
 
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
-import { store } from './store/store'
+import {store} from './store/store'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </HashRouter>
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
