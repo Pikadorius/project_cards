f9ee037eb275364ce14ee7560f4f6f605f201abc
Pikadorius <karpukea@yandex.by>
2023-02-09 09:48:22 +0300

    added base urls

---
+++ src/common/constans/url.ts
@@ -0,0 +1,9 @@
+export const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:7542/2.0/'
+    : 'https://neko-back.herokuapp.com/2.0/'
+
+export const recoveryURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/'
+    : 'https://pikadorius.github.io/project_cards/'
