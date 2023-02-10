603c6940e2848a10c704b945b07e6254c814bbef
Pikadorius <karpukea@yandex.by>
2023-02-10 20:49:53 +0300

    added packs API

---
+++ src/featuries/Packs/packsType.ts
@@ -0,0 +1,39 @@
+export type PacksQueryParamsType = {
+  packName: string
+  min: string
+  max: string
+  sortPacks: string
+  page: string
+  pageCount: string
+  user_id: string
+}
+
+export type PackType = {
+  _id: string
+  user_id: string
+  user_name: string
+  private: boolean
+  name: string
+  path: string
+  grade: number
+  shots: number
+  deckCover: string
+  cardsCount: number
+  type: string
+  rating: number
+  created: string
+  updated: string
+  more_id: string
+  __v: number
+}
+
+export type GetPacksResponseType = {
+  cardPacks: PackType[]
+  page: number
+  pageCount: number
+  cardPacksTotalCount: number
+  minCardsCount: number
+  maxCardsCount: number
+  token: string
+  tokenDeathTime: number
+}
