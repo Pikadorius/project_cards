be2f9a3792245ddcd864ec9a52f04d39631f20b5
Pikadorius <karpukea@yandex.by>
2023-02-11 11:05:52 +0300

    some packs type and api fixes

--- src/featuries/packs/packsAPI.ts
+++ src/featuries/packs/packsAPI.ts
@@ -3,6 +3,7 @@ import { instance } from '../../common/constans/instance'
 import { GetPacksResponseType, PacksQueryParamsType } from './packsType'
 
 export const packsAPI = {
-  getPacks: (params: PacksQueryParamsType) =>
-    instance.get<GetPacksResponseType>('cards/packs', { params }),
+  getPacks: (params: PacksQueryParamsType = {}) => {
+    return instance.get<GetPacksResponseType>('cards/pack', { params })
+  },
 }
