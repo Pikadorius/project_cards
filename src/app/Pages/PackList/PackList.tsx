453f5c0b57ad82b4e48a0919b8b578e09ed94b03
Pikadorius <karpukea@yandex.by>
2023-02-11 11:15:49 +0300

    test for packs

--- src/app/Pages/PackList/PackList.tsx
+++ src/app/Pages/PackList/PackList.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Navigate } from 'react-router-dom'
 
 import { PATH } from '../../../common/constans/path'
+import { useAppDispatch } from '../../../common/hooks/AppDispatch'
 import { useAppSelector } from '../../../common/hooks/AppSelector'
+import { fetchPacks } from '../../../featuries/packs/packsSlice'
 
 import s from './PackList.module.scss'
 
 export const PackList = () => {
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+  const packs = useAppSelector(state => state.packs.cardPacks)
+  const page = useAppSelector(state => state.packs.page)
+  const pageCount = useAppSelector(state => state.packs.pageCount)
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return
+    }
+    dispatch(fetchPacks({ pageCount: '20', packName: 'eng' }))
+  }, [])
 
   if (!isLoggedIn) {
     return <Navigate to={PATH.LOGIN} />
@@ -17,6 +31,9 @@ export const PackList = () => {
   return (
     <div className={s.container}>
       <h2>Pack List</h2>
+      {packs.map(p => {
+        return <div key={p._id}>{p.name}</div>
+      })}
     </div>
   )
 }
