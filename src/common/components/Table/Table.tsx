e7d0646ec965c94f5e9e561685c300a316770696
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-02-12 01:28:07 +0300

    fix and add icon study

--- src/common/components/Table/Table.tsx
+++ src/common/components/Table/Table.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import teacher from '../../../assets/teacher.svg'
 import { PackType } from '../../../featuries/packs/packsType'
 import { SortTitlePackListType } from '../../constans/sort'
+import { userNameHandler } from '../../utils/userNameHandler'
 
 import s from './Table.module.scss'
 import { Thead } from './Thead/Thead'
@@ -20,15 +22,17 @@ export const Table: React.FC<TableType> = ({ sortTitlePackList, packs, userId })
         <tbody>
           {packs.map(t => {
             const dateUpdate = t.updated.toString()
-            const dateCreated = t.created.toString()
+            const userName = userNameHandler(t.user_name)
 
             return (
               <tr key={t._id} className={s.tr}>
                 <td className={s.td}>{t.name}</td>
                 <td className={s.td}>{t.cardsCount}</td>
                 <td className={s.td}>{dateUpdate}</td>
-                <td className={s.td}>{dateCreated}</td>
-                <td className={s.td}>{t.user_id === userId ? 1 : 2}</td>
+                <td className={s.td}>{userName}</td>
+                <td className={s.td}>
+                  {t.user_id === userId ? 1 : <img src={teacher} alt="learn pack" />}
+                </td>
               </tr>
             )
           })}
--- src/common/utils/userNameHandler.ts
+++ src/common/utils/userNameHandler.ts
@@ -1,17 +1,12 @@
 export const userNameHandler = (userName: string) => {
-  const maxLength = 22
-  const cutIndex = userName.indexOf('@') !== -1 ? userName.indexOf('@') : maxLength
-  const newUserName = `${userName
-    .split('')
-    .map((el, i) => {
-      const firstLetter = el.toUpperCase()
+  const max = 22
+  const name = userName.split('@')[0]
 
-      if (i === 0) return firstLetter
-
-      return i >= cutIndex || i >= maxLength ? null : el
-    })
-    .filter(el => el !== null)
-    .join('')}`
-
-  return userName.length > maxLength ? `${newUserName}...` : newUserName
+  return name.length <= max
+    ? name
+    : name
+        .split('')
+        .map((el, i) => (i <= max ? el : null))
+        .filter(el => el)
+        .join('') + '...'
 }
