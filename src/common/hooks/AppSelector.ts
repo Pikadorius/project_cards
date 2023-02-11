e0a189f265a8281d3c00bf54414d972a87f9b9bb
Pikadorius <karpukea@yandex.by>
2023-02-11 14:05:24 +0300

    added Type to RootState

--- src/common/hooks/AppSelector.ts
+++ src/common/hooks/AppSelector.ts
@@ -2,5 +2,5 @@ import { TypedUseSelectorHook, useSelector } from 'react-redux'
 
 import { store } from '../../store/store'
 
-export type RootState = ReturnType<typeof store.getState>
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export type RootStateType = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
