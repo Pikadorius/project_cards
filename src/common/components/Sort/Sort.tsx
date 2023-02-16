e5841c529b6b44053e1954bab76fc41dbf9d15b1
Pikadorius <karpukea@yandex.by>
2023-02-16 14:22:23 +0300

    renamed component

--- src/common/components/Sort/Sort.tsx
+++ src/common/components/Sort/Sort.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ButtonGroupe } from '../ButtonGroupe/ButtonGroupe'
+import { ButtonGroup } from '../ButtonGroupe/ButtonGroup'
 import { Range } from '../Range/Range'
 import { ResetSort } from '../ResetSort/ResetSort'
 
@@ -9,7 +9,7 @@ import s from './Sort.module.scss'
 export const Sort = () => {
   return (
     <div className={s.container}>
-      <ButtonGroupe />
+      <ButtonGroup />
       <Range />
       <ResetSort />
     </div>
