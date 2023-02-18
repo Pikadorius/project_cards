150453e0c2271a6f76d5dfa315a0dd8ae5855111
Pikadorius <karpukea@yandex.by>
2023-02-18 10:51:00 +0300

    ModalWrapper test

--- src/common/components/ModalWrapper/ModalWrapper.tsx
+++ src/common/components/ModalWrapper/ModalWrapper.tsx
@@ -6,17 +6,22 @@ import IconButton from '@mui/material/IconButton'
 
 import s from './ModalWrapper.module.scss'
 
+import { setModalActive } from 'app/appSlice'
+import { useAppDispatch } from 'common/hooks'
+
 type PropsType = {
   children?: ReactNode
   title: string
 }
 const ModalWrapper: FC<PropsType> = ({ children, title }) => {
+  const dispatch = useAppDispatch()
+
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
         <div className={s.modalTitle}>
           {title}
-          <IconButton aria-label="delete">
+          <IconButton aria-label="delete" onClick={() => dispatch(setModalActive(false))}>
             <CloseIcon />
           </IconButton>
         </div>
