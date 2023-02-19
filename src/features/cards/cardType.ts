8e58eccb88072611528d7ec194c1599919abe780
Pikadorius <karpukea@yandex.by>
2023-02-19 17:20:09 +0300

    changed types

--- src/features/cards/cardType.ts
+++ src/features/cards/cardType.ts
@@ -53,14 +53,14 @@ export type CreateCardRequestType = {
 
 export type CreateCardType = {
   cardsPack_id: string
-  question: string
-  answer: string
-  grade: number
-  shots: number
-  answerImg: string
-  questionImg: string
-  questionVideo: string
-  answerVideo: string
+  question?: string
+  answer?: string
+  grade?: number
+  shots?: number
+  answerImg?: string
+  questionImg?: string
+  questionVideo?: string
+  answerVideo?: string
 }
 
 export type UpdateCardObjType = {
