02768c0956074e4a60eb3cd66e45a37ebb8b931b
denbarabraza <denis.bareischev@gmail.com>
2023-02-22 00:56:26 +0300

    changed grades page

--- src/features/cards/LearnCardPage/LearnCardItem/GradesItem/GradesItem.tsx
+++ src/features/cards/LearnCardPage/LearnCardItem/GradesItem/GradesItem.tsx
@@ -1,5 +1,7 @@
-import React, { FC, memo } from 'react'
+import React, { FC, memo, useState } from 'react'
 
+import star from '../../../../../assets/star.png'
+import starFull from '../../../../../assets/starFull.png'
 import { useAppSelector } from '../../../../../common/hooks'
 import { AnswerStatuses } from '../../learnCardSlice'
 
@@ -10,31 +12,43 @@ type GradesItemType = {
 }
 
 export const GradesItem: FC<GradesItemType> = memo(({ onChangeChecked }) => {
+  const [starValue, setStarValue] = useState<boolean>(true)
+
   const gradesCardLearn = useAppSelector(state => state.learnCard)
+
+  const onClickImg = () => {
+    setStarValue(false)
+  }
 
   return (
     <div className={s.gradesItem}>
-      <div className={s.grades}>Rate yourself:</div>
-      {gradesCardLearn.map((g, i) => {
-        return (
-          <div key={i} className={s.inputItem}>
-            <label>
-              <input
-                type={'checkbox'}
-                className={s.customCheckbox}
-                checked={g.status === AnswerStatuses.IsActive}
-                onChange={e =>
-                  onChangeChecked(
-                    e.currentTarget.checked ? AnswerStatuses.IsActive : AnswerStatuses.IsNoActive,
-                    g.id
-                  )
-                }
-              />
-              <span>{g.title}</span>
-            </label>
-          </div>
-        )
-      })}
+      <h3 className={s.grades}>Rate yourself:</h3>
+      <div className={s.gradesBlock}>
+        {gradesCardLearn.map((g, i) => {
+          return (
+            <div key={i} className={s.inputItem}>
+              <label>
+                <input
+                  type={'checkbox'}
+                  className={s.customCheckbox}
+                  checked={g.status === AnswerStatuses.IsActive}
+                  onClick={onClickImg}
+                  title={g.title}
+                  onChange={e =>
+                    onChangeChecked(
+                      e.currentTarget.checked ? AnswerStatuses.IsActive : AnswerStatuses.IsNoActive,
+                      g.id
+                    )
+                  }
+                />
+                <span>{g.title}</span>
+                {/*{star && <img src={star} className={s.star} title={g.title} />}
+                {!star && <img src={starFull} className={s.star} title={g.title} />}*/}
+              </label>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 })
